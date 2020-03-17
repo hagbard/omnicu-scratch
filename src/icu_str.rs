4459a2494b2096eb6e70d91cfc50f547b6161ea8
hagbard <david.beaumont+github@gmail.com>
2020-03-17 21:16:46 +0100

    Baby steps...

---
+++ src/icu_str.rs
@@ -0,0 +1,24 @@
+trait IcuStr {
+  type Iter: Iterator<Item = (usize, char)>;
+
+  fn icu_chars(&self, n: usize) -> Self::Iter;
+}
+
+impl<'a> IcuStr for &'a str {
+  type Iter = std::str::CharIndices<'a>;
+
+  #[inline]
+  fn icu_chars(&self, n: usize) -> Self::Iter {
+    self[n..].char_indices()
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::IcuStr;
+
+  #[test]
+  fn do_stuff() {
+    "abcdef".icu_chars(2).for_each(|(n,c)| println!("{:?}", c));
+  }
+}
