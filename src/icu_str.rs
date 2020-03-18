8846b3f2c2aaec08597e88a0a47af5e195a195f2
hagbard <david.beaumont+github@gmail.com>
2020-03-18 14:40:14 +0100

    First utf16 working

--- src/icu_str.rs
+++ src/icu_str.rs
@@ -15,20 +15,32 @@ impl<'a, T: AsRef<str>> IcuStrRef<'a> for &'a T {
   }
 }
 
-//pub trait IcuStr {
-//  type Iter: Iterator<Item = (usize, char)>;
-//
-//  fn icu_chars(&self, n: usize) -> Self::Iter;
-//}
-//
-//impl<'a> IcuStr for &'a str {
-//  type Iter = CharIndices<'a>;
-//
-//  #[inline]
-//  fn icu_chars(&self, n: usize) -> Self::Iter {
-//    self[n..].char_indices()
-//  }
-//}
+pub struct Utf16CharIndices<'a> {
+  utf16: &'a [u16],
+  n: usize,
+}
+
+impl <'a> Iterator for Utf16CharIndices<'a> {
+  type Item = (usize, char);
+
+  fn next(&mut self) -> Option<Self::Item> {
+    if self.n < self.utf16.len() {
+      let v = self.utf16[self.n];
+      let c: (usize, char) = (self.n, unsafe { std::char::from_u32_unchecked(v as u32) });
+      self.n += 1;
+      return Some(c)
+    }
+    None
+  }
+}
+
+impl<'a> IcuStrRef<'a> for &'a [u16] {
+  type Iter = Utf16CharIndices<'a>;
+
+  fn icu_chars(self, n: usize) -> Self::Iter {
+    Utf16CharIndices{ utf16: &self[n..], n: 0 }
+  }
+}
 
 #[cfg(test)]
 mod tests {
@@ -37,5 +49,12 @@ mod tests {
   #[test]
   fn do_stuff() {
     &"abcdef".icu_chars(2).for_each(|(n,c)| println!("{:?}", c));
+  }
+
+  #[test]
+  fn do_stuff_utf16() {
+    let v: Vec<u16> = vec!(65, 66, 67, 68, 69);
+    v.as_slice().icu_chars(2).for_each(|(n,c)| println!("{:?}", c));
+    assert!(false)
   }
 }
